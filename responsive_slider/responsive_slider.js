dd201b90f155647bef8c7ba4c865a978e6530d30
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-10-22 14:13:05 -0300

    Improve UX to slider

--- responsive_slider/responsive_slider.js
+++ responsive_slider/responsive_slider.js
@@ -12,7 +12,19 @@ var swiper = new Swiper(".mySwiper", {
     },
     loop: true,
     autoplay: {
-        delay: 3000,
+        delay: 2000,
         disableOnInteraction: false,
-    }
+        pauseOnMouseEnter: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: 'true',
+      renderBullet: (index, className) => {
+      return '' + '' + '' + '' + '' + '';
+      }
+    },
+    mousewheel: {
+      releaseOnEdges: true,
+    },
   });
